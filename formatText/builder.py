cfa5391141db7758cb5117f69161c54533f2b0f7
Slfftz520 <slfftz520@163.com>
2026-08-25 15:14:30 +0800

    更新 builder.py

--- formatText/builder.py
+++ formatText/builder.py
@@ -1,21 +1,24 @@
 
 import json
-from parser import parse_line, strip_json_keys
+from .parser import parse_line, strip_json_keys
 
 def generate_commands(text: str, pretty: bool = False) -> list[str]:
-    """将多行文本转换为 tellraw 命令列表"""
+    """将多行文本转换为 tellraw 命令列表，每条命令的第一个组件均为空文本（防止样式继承）"""
     lines = text.split('\n')
     commands = []
     for line in lines:
         if line == '':
-            components = [{'text': ''}]
+            parsed = [{'text': ''}]
         else:
-            components = parse_line(line)
-            if not components:
-                components = [{'text': ''}]
-        # 序列化
+            parsed = parse_line(line)
+            if not parsed:
+                parsed = [{'text': ''}]
+        # 强制以空组件开头，重置样式
+        components = [{'text': ''}] + parsed
+
         if pretty:
             json_str = json.dumps(components, ensure_ascii=False, indent=2)
+            json_str = ' '.join(json_str.split())   # 压缩为单行
         else:
             json_str = json.dumps(components, ensure_ascii=False)
         json_str = strip_json_keys(json_str)
